11128db589791fe9e43d7b4165126f261ee1efe3
Anvesh Dange <anveshdange19@gmail.com>
2022-04-02 22:47:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import data from "./data"
+import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+
+  const cards = data.map(item =>{
+    return (
+      <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+    )
+  })
+
+  return(
+    <div >
+    <Navbar/>
+    <Hero/>
+    <section className="cards-list">
+    {cards}
+    </section>
+    <Footer/>
+    
     </div>
-  );
-}
+  )
 
-export default App;
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React  from "react";
+
+export default function Footer(){
+    return(<div className="footer">
+        <h5>Made with ❤️ by </h5>
+        <h5><a href="https://bit.ly/anveshdange"> * Anvesh Dange *</a></h5>
+    </div>)
+}
